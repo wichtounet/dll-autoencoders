04b2f596f8877c2812ad2c33b435b7c0891fddd3
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-12-29 15:44:08 +0100

    Cleanup code style

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
-#include <iostream>
-#include <chrono>
 #include <atomic>
+#include <chrono>
+#include <iostream>
 
-#include "dll/neural/dense_layer.hpp"
 #include "dll/dbn.hpp"
+#include "dll/neural/dense_layer.hpp"
 #include "dll/trainer/stochastic_gradient_descent.hpp"
 
 #include "mnist/mnist_reader.hpp"
@@ -20,8 +20,8 @@ namespace {
 
 constexpr const size_t K = 3;
 
-template<typename C>
-float distance_knn(const C& lhs, const C& rhs){
+template <typename C>
+float distance_knn(const C& lhs, const C& rhs) {
     return etl::sum((lhs - rhs) >> (lhs - rhs));
 }
 
@@ -30,17 +30,17 @@ struct distance_t {
     uint32_t label;
 };
 
-size_t vote_knn(const std::vector<distance_t>& distances){
+size_t vote_knn(const std::vector<distance_t>& distances) {
     size_t votes[10]{};
 
-    for(size_t k = 0; k < K; ++k){
+    for (size_t k = 0; k < K; ++k) {
         ++votes[distances[k].label];
     }
 
     size_t label = 0;
 
-    for(size_t k = 1; k < 10; ++k){
-        if(votes[k] > votes[label]){
+    for (size_t k = 1; k < 10; ++k) {
+        if (votes[k] > votes[label]) {
             label = k;
         }
     }
@@ -48,27 +48,27 @@ size_t vote_knn(const std::vector<distance_t>& distances){
     return label;
 }
 
-template<typename C, typename L>
-double evaluate_knn(const C& training, const C& test, const L& training_labels, const L& test_labels){
+template <typename C, typename L>
+double evaluate_knn(const C& training, const C& test, const L& training_labels, const L& test_labels) {
     std::atomic<size_t> correct;
 
     correct = 0;
 
     cpp::default_thread_pool<> pool(8);
 
-    cpp::parallel_foreach_n(pool, 0, test.size(), [&](const size_t i){
+    cpp::parallel_foreach_n(pool, 0, test.size(), [&](const size_t i) {
         std::vector<distance_t> distances(training.size());
 
-        for(size_t j = 0; j < training.size(); ++j){
-            float d = distance_knn(test[i], training[j]);
+        for (size_t j = 0; j < training.size(); ++j) {
+            float d      = distance_knn(test[i], training[j]);
             distances[j] = {d, training_labels[j]};
         }
 
-        std::sort(distances.begin(), distances.end(), [](const distance_t& lhs, const distance_t& rhs){
+        std::sort(distances.begin(), distances.end(), [](const distance_t& lhs, const distance_t& rhs) {
             return lhs.dist < rhs.dist;
         });
 
-        if(vote_knn(distances) == test_labels[i]){
+        if (vote_knn(distances) == test_labels[i]) {
             ++correct;
         }
     });
@@ -76,17 +76,16 @@ double evaluate_knn(const C& training, const C& test, const L& training_labels, 
     return correct / double(test.size());
 }
 
-template<size_t I, typename N, typename D>
-double evaluate_knn_net(const N& net, const D& dataset){
-
+template <size_t I, typename N, typename D>
+double evaluate_knn_net(const N& net, const D& dataset) {
     std::vector<etl::dyn_vector<float>> training(dataset.training_images.size());
     std::vector<etl::dyn_vector<float>> test(dataset.test_images.size());
 
-    for(std::size_t i = 0; i < training.size(); ++i){
+    for (std::size_t i = 0; i < training.size(); ++i) {
         training[i] = net->template features_sub<I>(dataset.training_images[i]);
     }
 
-    for(std::size_t i = 0; i < test.size(); ++i){
+    for (std::size_t i = 0; i < test.size(); ++i) {
         test[i] = net->template features_sub<I>(dataset.test_images[i]);
     }
 
@@ -95,21 +94,21 @@ double evaluate_knn_net(const N& net, const D& dataset){
 
 } // end of anonymous
 
-int main(int argc, char* argv []) {
+int main(int argc, char* argv[]) {
     std::string model = "raw";
-    if(argc > 1){
+    if (argc > 1) {
         model = argv[1];
     }
 
     auto dataset = mnist::read_dataset_direct<std::vector, etl::dyn_vector<float>>();
 
-    if(model == "raw"){
+    if (model == "raw") {
         double accuracy = evaluate_knn(dataset.training_images, dataset.test_images, dataset.training_labels, dataset.test_labels);
         std::cout << "Raw: " << accuracy << std::endl;
-    } else if(model == "dense"){
+    } else if (model == "dense") {
         mnist::binarize_dataset(dataset);
 
-#define SINGLE_AE(N)                                                             \
+#define SINGLE_AE(N)                                                                 \
         {                                                                            \
             using network_t =                                                        \
                 dll::dbn_desc<dll::dbn_layers<dll::dense_desc<28 * 28, N>::layer_t,  \
